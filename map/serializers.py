d68cb2dad8eddaa18655135ae9b8ee299e632e06
angevoit <voit@umich.edu>
2026-03-26 21:40:52 -0500

    django start

--- map/serializers.py
+++ map/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
+from django.http import JsonResponse
+import logging
 
 from map.models import CommunityArea, RestaurantPermit
-
 
 class CommunityAreaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,9 +11,11 @@ class CommunityAreaSerializer(serializers.ModelSerializer):
 
     num_permits = serializers.SerializerMethodField()
 
+    
+
     def get_num_permits(self, obj):
         """
-        TODO: supplement each community area object with the number
+        TODO: supplement each community area object with the numbesr
         of permits issued in the given year.
 
         e.g. The endpoint /map-data/?year=2017 should return something like:
@@ -30,5 +33,10 @@ class CommunityAreaSerializer(serializers.ModelSerializer):
             }
         ]
         """
+        permit_counter = 0
+        # TODO add year filtering
+        for permit in RestaurantPermit.objects.all():
+            if permit.community_area_id == self.Meta.model.area_id:
+                permit_counter += 1
 
-        pass
+        return permit_counter
